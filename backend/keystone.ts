9143049a90af67e597894961c582dbd9edf22105
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2021-06-20 16:08:05 -0300

    [dev] - Finished Module # 4 (Server Side GraphQL Development) - TODO: Next is Module # 5 (Client Side React + GraphQL Development)
    On branch dev
    Changes to be committed:
    	modified:   backend/keystone.ts
    	modified:   backend/package.json
    	new file:   backend/schemas/Product.ts
    	new file:   backend/schemas/ProductImage.ts
    	modified:   backend/seed-data/index.ts
    	modified:   backend/yarn.lock

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,9 @@ import {
 	statelessSessions,
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone';
 
@@ -35,10 +38,17 @@ export default withAuth(
 		db: {
 			adapter: 'mongoose',
 			url: databaseURL,
-			// TODO: Add data seeding here
+			async onConnect(keystone) {
+				console.log('Connected to the database');
+				if (process.argv.includes('--seed-data')) {
+					await insertSeedData(keystone);
+				}
+			},
 		},
 		lists: createSchema({
 			User,
+			Product,
+			ProductImage,
 			// Schema items go in here
 		}),
 		ui: {
