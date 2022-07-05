36d60fffbb752591a9513d676b0e0b5adfd5e948
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2022-07-05 20:25:50 -0300

    Updates prodEndpoint

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,5 +1,5 @@
 export const devEndpoint =
 	process.env.GRAPHQL_ENDPOINT || `http://localhost:3000/api/graphql`;
 export const prodEndpoint =
-	process.env.GRAPHQL_ENDPOINT || `http://TODO:prodEndpoint/api/graphql`;
+	process.env.GRAPHQL_ENDPOINT || `https://grifecommerce-backend-95gmymxkz-diegovfeder.vercel.app/api/graphql`;
 export const totalProductsPerPage = 2;
