fcf9052b12e7c5d514ed7236b9a2eff707cd7836
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2022-07-05 09:26:08 -0300

    Updates next.config.js to remove test files from pages dir.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,14 @@ module.exports = {
 	images: {
 		domains: ['res.cloudinary.com'],
 	},
-	webpack: config => {
+	pageExtensions: ['page.js', 'page.jsx', 'page.ts', 'page.tsx'],
+	webpack: (config, { webpack }) => {
 		config.module.rules.push({
 			test: /\.(graphql|gql)$/,
 			exclude: /node_modules/,
 			loader: 'graphql-tag/loader',
 		});
+		config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
 		return config;
 	},
 	// FIXME: What does this do?
