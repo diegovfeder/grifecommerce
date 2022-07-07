d50f552d7444ebd3107cc302dbebd7f97ec83bd6
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2022-07-07 08:30:29 -0300

    Updates _document.tsx to jsx. Removes type enforcement.

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -3,12 +3,11 @@ import Document, {
 	Head,
 	NextScript,
 	Main,
-	DocumentContext,
 } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
+	static async getInitialProps(ctx) {
 		const sheet = new ServerStyleSheet();
 		const originalRenderPage = ctx.renderPage;
 
@@ -32,7 +31,7 @@ export default class MyDocument extends Document {
 			sheet.seal();
 		}
 	}
-	
+
 	render() {
 		return (
 			<Html lang="pt-BR">
